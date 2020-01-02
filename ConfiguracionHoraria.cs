f78d0fc82af41a0d06419323730db642e6f601ca
BiruCode <yeisonplox@gmail.com>
2020-01-02 13:47:47 -0500

    Proyecto Reloj tipo 12 horas con mensajes segun si es de noche, tarde o mañana

---
+++ ConfiguracionHoraria.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoReloj
+{
+    class ConfiguracionHoraria
+    {
+        public byte bHora, bMinuto, bSegundo;//creacion de variables necesarias
+        public string sPeriodo;
+        public void ZonaHoraria()
+        {
+            int z = 0;//variable necesaria para logica
+            #region Explicacion y solicitud de datos
+            while (z==0)//mientras z sea igual a 0 repita infinitamente lo siguiente
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow; //Color de las letras
+                Console.SetCursorPosition(25, 12);//ubicacion en consola
+                Console.WriteLine("         Porfavor a continuacion configura el reloj segun tu zona horaria");
+                Console.SetCursorPosition(25, 13);
+                Console.WriteLine("Agrega a continuacion hora (formato 12 horas), minutos, segundos y periodo(AM o PM).");
+                Console.SetCursorPosition(25, 14);
+                Console.Write("             Presiona para continuar con la configuracion.");
+                Console.ReadKey();//esperar entrada por teclado
+
+                #region solicitud de datos
+                try//intentar lo siguiente...
+                {
+                    Console.SetCursorPosition(45, 16);
+                    Console.Write("Hora: ");//pedir hora
+                    bHora = byte.Parse(Console.ReadLine());//convertirla en byte y guardarlo en una variable
+
+                    Console.SetCursorPosition(45, 16);
+                    Console.Write("Minuto: ");
+                    bMinuto = byte.Parse(Console.ReadLine());
+
+                    Console.SetCursorPosition(45, 16);
+                    Console.Write("  Segundo: ");
+                    bSegundo = byte.Parse(Console.ReadLine());
+
+                    Console.SetCursorPosition(45, 16);
+                    Console.Write("Periodo(PM o AM): ");//pedir periodo
+                    sPeriodo = Console.ReadLine();//guardar en variable
+                    if (sPeriodo == "am" || sPeriodo == "Am")//condicional para guardar el periodo en mayuscula
+                    {
+                        sPeriodo = "AM";
+                        z = 1;//se le da a z = a 1 porque si pudo realizarse
+                    }
+                    else if (sPeriodo == "pm" || sPeriodo == "Pm")//condicional para guardar el periodo en mayuscula
+                    {
+                        sPeriodo = "PM";
+                        z = 1;//se le da a z = a 1 porque si pudo realizarse
+                    }
+                    else if (sPeriodo != "AM" || sPeriodo != "PM")//si lo que ingrese el usuario es diferente a lo que se le pide
+                    {
+                        Console.SetCursorPosition(45, 18);
+                        Console.WriteLine("No ingresaste el dato correcto");//diga que no ingreso lo correcto
+                        Console.ReadKey();//entrada por teclado
+                        z = 0;//se le da a z = 0 lo que significa que se repetira la solicitud de la configuracion
+                    }
+                }
+                catch //sino pudo realizar las conversiones o el usuario no ingreso datos correctos
+                {
+                    Console.Clear();//limpiar pantalla
+                    Console.SetCursorPosition(45, 15);
+                    Console.WriteLine("Hubo un error!No ingresaste los datos correctos");//imprimir que hubo un error
+                    z = 0;//se le da a z = 0 lo que significa que se repetira la solicitud de la configuracion
+                    Console.ReadKey();//entrada por teclado
+                    Console.Clear();//limpiar pantalla
+                }
+                Console.Clear();//limpiar pantalla
+                #endregion
+            }
+            Console.Clear();//limpiar pantalla
+            #endregion
+        }
+    }
+}
---
+++ Reloj.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;//libreria necesaria para el reloj
+
+namespace ProyectoReloj
+{
+    class Reloj : ConfiguracionHoraria//la clase "Reloj" heredará todos los atributos y metodos de la clase "ConfiguracionHoraria"
+    {
+        public Reloj()//constructor 
+        {
+            ZonaHoraria();//llamado del metodo de la clase ConfiguracionHoraria cuya funcion es configurar el reloj            
+            RelojDigital();//llamado del metodo ReloDigital cuya funcion es mostrar el reloj en pantalla con grafico
+        }
+        public void Grafico()
+        {
+            #region Grafico tipo RelojDigital
+            Console.ForegroundColor = ConsoleColor.Cyan;//color de las letras, nuemros etc
+            Console.SetCursorPosition(50, 10);//ubicacion en pantalla
+            Console.WriteLine("╔");//impresion
+            Console.SetCursorPosition(50, 11);
+            Console.WriteLine("║");
+            Console.SetCursorPosition(50, 12);
+            Console.WriteLine("╚");
+
+            Console.SetCursorPosition(62, 10);
+            Console.WriteLine("╗");
+            Console.SetCursorPosition(62, 11);
+            Console.WriteLine("║");
+            Console.SetCursorPosition(62, 12);
+            Console.WriteLine("╝");
+
+            int x = 51;//ubicacion en x necesaria
+            for (int i = 0; i < 4; i++)//ciclo for 
+            {
+                Console.SetCursorPosition(x, 10);
+                Console.WriteLine("═");
+                Console.SetCursorPosition(x + 1, 10);//ubicacion x+1
+                Console.WriteLine("═");//imprimir
+
+                Console.SetCursorPosition(x, 12);//se le da 2 valores mas a Y
+                Console.WriteLine("═");
+                Console.SetCursorPosition(x + 1, 12);
+                Console.WriteLine("═");
+                x = x + 3;//se suma x por 3
+            }
+
+            x = 53;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.SetCursorPosition(x, 10);
+                Console.WriteLine("╦");
+                Console.SetCursorPosition(x, 12);
+                Console.WriteLine("╩");
+                Console.SetCursorPosition(x, 11);
+                Console.WriteLine(":");
+
+                x = x + 3;
+            }
+            #endregion
+        }
+
+        public void RelojDigital()
+        {
+            int x = 0, y = 1;//variables necesarias para la logica del reloj
+
+            while (x == 0)//mientras x siempre sea 0, esto es para que el reloj siempre funcione asi acaben los ciclos
+            {
+                for (int hrs = bHora; hrs <= 12; hrs++)//ciclo for para las horas se le pasa la hora que ingreso el usuario
+                {
+                    
+
+                    for (int min = bMinuto; min < 60; min++)//ciclo for para los minutos y se le pasa los minutos que ingreso el usuario
+                    {
+                        if (bMinuto == 59)//daba un error cuando se ingresaba por ejemplo 11:59:55 cuando pasaba a las 12 pasaba de la siguiente manera 12:59:00
+                            bMinuto = 0;//esta condicional arregla el error. Si minuto es igual a 59 entonces que pase directamente a 0
+                        if (y == 0)//Condicional para no se repita infinitamente el segundo que ingreso el usuario, si y es igual a 0
+                            bSegundo = 0;//elsegundo comenzara en 0
+                        for (int seg = bSegundo; seg < 60; seg++)//ciclo for para los segundos y se le pasa los segundos que ingreso el usuario
+                        {
+                            if (hrs == 11&&min==59&&seg==59)//cuando sean las 11 y 59 y 59 segundos
+                            {
+                                if (sPeriodo == "am" || sPeriodo == "AM" || sPeriodo == "Am")//y si el usuario ingreso que es de mañana
+                                    sPeriodo = "PM";//entonces que cambie a pm
+                                else if (sPeriodo == "pm" || sPeriodo == "PM" || sPeriodo == "Pm")//sino
+                                    sPeriodo = "AM";//que cambie a am
+                            }
+
+                            Console.Clear();//limpiara cada vez que aumente de segundo 
+                            #region mensajes
+                            if (sPeriodo=="AM")//si son las am
+                            {
+                                Console.SetCursorPosition(45,14);//imprimira el siguientes mensaje en estas ubicaciones
+                                Console.WriteLine("Comenzar tu dia con una sonrisa");
+                                Console.SetCursorPosition(45, 15);
+                                Console.WriteLine("hara que tu destino se pinte");
+                                Console.SetCursorPosition(45, 16);
+                                Console.WriteLine("de colores. ¡Buenos Dias!");
+                            }
+                            else if (sPeriodo == "PM")//sino si son las PM
+                            {
+                                Console.SetCursorPosition(40, 14);//imprima el siguiente mensaje en las siguientes posiciones
+                                Console.WriteLine("Aprende del ayer. Pero vive siempre");
+                                Console.SetCursorPosition(40, 15);
+                                Console.WriteLine("en el dia a dia, mira hacia el horizonte");
+                                Console.SetCursorPosition(40, 16);
+                                Console.WriteLine("del mañana esta bella tarde que nos acompaña");
+                                Console.SetCursorPosition(50, 17);
+                                Console.WriteLine("¡Feliz Tarde!");
+                            }
+                            if (hrs>=6&&hrs!=12&&sPeriodo == "PM")//si son mas  de las 6 pm
+                            {
+                                Console.Clear();//limpie el mensaje de feliz tarde 
+                                Console.SetCursorPosition(40, 14);//imprimir el siguiente mensaje en las siguientes posiciones
+                                Console.WriteLine("Cuando te vayas a la cama ");
+                                Console.SetCursorPosition(40, 15);
+                                Console.WriteLine("no olvides dejar tus problemas");
+                                Console.SetCursorPosition(40, 16);
+                                Console.WriteLine("bien lejos. ¡Feliz Noche!");                               
+                            }
+                            #endregion
+                            Grafico();//imprimira infinitamente el grafico
+                            Console.SetCursorPosition(51, 11);//ubicacion dentro del grafico
+                            Console.Write(hrs);//se imprimira la hora
+                            Console.SetCursorPosition(54, 11);//ubicacion dentro del grafico
+                            Console.Write(min);//imprimira minuto
+                            Console.SetCursorPosition(57, 11);//posicion dentro del grafico
+                            Console.Write(seg);//impresion segundos
+                            Console.SetCursorPosition(60, 11);//ubicacion dentro del grafico
+                            Console.Write(sPeriodo);//impresion periodo
+                            Thread.Sleep(1000);//tiempo equivalente a 1 segundo
+                            y = 0;//cuando llega a 59 y sera = a 0                            
+                        }
+                    }
+                }
+                //cuando termine de hacer todos los ciclos
+                bHora = 1;//hora equivale a 1
+                bMinuto = 0;//minuto a 0
+                bSegundo = 0;//segundo a 0
+            }
+        }
+        
+    }
+}
